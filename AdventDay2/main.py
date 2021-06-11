9e35062227bd54fdc4a8cf2b28865dbf1ff94e9e
myleslangston <84974498+myleslangston@users.noreply.github.com>
2021-06-11 17:52:43 +0100

    Day 2 Advent commit

--- AdventDay2/main.py
+++ AdventDay2/main.py
@@ -1,9 +1,47 @@
-def day2(arr):
-    count = 0
-    for i in arr:
-        for l in arr:
-            if i in arr and l == range(i):
-                count += 1
-                print(count)
+with open("/Users/myleslangston/Documents/Sparta Global /day2.txt", "r") as fp:
+    num = fp.readlines()
 
-day2([["1-3", "a", "abcde"], ["1-3", "b", "cdefg"], ["2-9", "c", "ccccccccc"]])
+
+valid_passwords = 0
+for line in num:  # break list up into further lists
+    data = line.split(", ")  # "1-3 f: gvfghfdgsgs"
+    count = 0
+    for char in data:
+        data = char.replace('\n', '')
+        data = list(data)
+        letter = (data[data.index(":") - 1])
+        password = data[-1:data.index(':'):-1]
+
+
+        if '-' == data[1]:
+            minimum = int(data[0])
+
+            if ' ' == data[3]:
+                maximum = int(data[2])
+                for i in password:
+                    if i == letter:
+                        count += 1
+                if count in range(minimum, maximum+1):
+                    valid_passwords += 1
+
+            elif ' ' == data[4]:
+                maximum = int(data[2] + data[3])
+                for i in password:
+                    if i == letter:
+                        count += 1
+                if count in range(minimum, maximum+1):
+                    valid_passwords += 1
+
+        elif '-' == data[2]:
+            minimum = int(data[0] + data[1])
+
+            if ' ' == data[5]:
+                maximum = int(data[3] + data[4])
+                for i in password:
+                    if i == letter:
+                        count += 1
+                if count in range(minimum, maximum+1):
+                    valid_passwords += 1
+
+print(valid_passwords)
+
